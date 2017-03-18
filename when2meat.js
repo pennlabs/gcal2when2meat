648ec7e2af171c764f23b6ad5defc09859188cd3
Adam Domingoes <adamdom@wharton.upenn.edu>
2017-03-18 13:27:50 -0400

    change body selection

--- when2meat.js
+++ when2meat.js
@@ -4,7 +4,9 @@ document.getElementsByTagName('head')[0].appendChild(jq);
 
 const baconPattern = "https://s3.amazonaws.com/spoonflower/public/design_thumbnails/0196/0562/rrbacon_1_shop_thumb.png"
 
-  $("#MainBody").css("background", "linear-gradient(rgba(255, 255, 255, 0.85),rgba(255, 255, 255, 0.85)),url('https://3.bp.blogspot.com/-0aphAH5D7DA/V5-0Y9b14oI/AAAAAAAABTI/sUU1fwneTtI8GmI0H8aT00AIe-dzJrGmACLcB/s1600/Web-Article-Chef-Marcus-Samuelsson-Streetbird-Red-Rooster-Harlem-Tips-on-Roasting-Brining-a-Perfect-Chicken-Rotisserie-Recipe1.gif')")
+  $("body").css("background", "linear-gradient(rgba(255, 255, 255, 0.85),rgba(255, 255, 255, 0.85)),url('https://3.bp.blogspot.com/-0aphAH5D7DA/V5-0Y9b14oI/AAAAAAAABTI/sUU1fwneTtI8GmI0H8aT00AIe-dzJrGmACLcB/s1600/Web-Article-Chef-Marcus-Samuelsson-Streetbird-Red-Rooster-Harlem-Tips-on-Roasting-Brining-a-Perfect-Chicken-Rotisserie-Recipe1.gif')")
+  
+  
   $("[bgcolor]").css("background", "rgb(255,255,255)")
   $("[bgcolor='#339900']").css("background", "url('" + baconPattern + "')")
 
