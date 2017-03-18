0bb230071aea244e8da8e5abb921caf176325ba5
Adam Domingoes <adamdom@wharton.upenn.edu>
2017-03-18 13:08:47 -0400

    td typo

--- when2meat.js
+++ when2meat.js
@@ -9,20 +9,20 @@ setInterval(function(){
   $("#GroupGrid > div:last-child > div[id!='GroupSlots']").css("background", "rgb(255,255,255)")
 
 
-  $("td[id*='GroupTime'][style*='51, 153, 0']").css("background", "rgba(0,0,0,0)")
-  $("td[id*='GroupTime'][style*='339900']").css("background", "rgba(0,0,0,0)")
+  $("[id*='GroupTime'][style*='51, 153, 0']").css("background", "rgba(0,0,0,0)")
+  $("[id*='GroupTime'][style*='339900']").css("background", "rgba(0,0,0,0)")
   
   
   $("#YouGrid > div:last-child").css("background", "url('" + baconPattern  + "')")
   $("#YouGrid > div:last-child > div[id!='GroupSlots']").css("background", "rgb(255,255,255)")
   
   
-  $("td[id*='YouTime']:not([style*='339900'], [style*='255,222,222'])").css("background", "rgba(0,0,0,0)")
-  $("td[id*='YouTime'][style*='255, 222, 222']").css("background", "rgb(255,255,255)")
+  $("[id*='YouTime']:not([style*='339900'], [style*='255,222,222'])").css("background", "rgba(0,0,0,0)")
+  $("[id*='YouTime'][style*='255, 222, 222']").css("background", "rgb(255,255,255)")
 
   
-  $("td[bgcolor]").css("background", "rgb(255,255,255)")
-  $("td[bgcolor='#339900']").css("background", "url('" + baconPattern + "')")
+  $("[bgcolor]").css("background", "rgb(255,255,255)")
+  $("[bgcolor='#339900']").css("background", "url('" + baconPattern + "')")
 
   $("#MainBody").css("background", "linear-gradient(rgba(255, 255, 255, 0.85),rgba(255, 255, 255, 0.85)),url('https://3.bp.blogspot.com/-0aphAH5D7DA/V5-0Y9b14oI/AAAAAAAABTI/sUU1fwneTtI8GmI0H8aT00AIe-dzJrGmACLcB/s1600/Web-Article-Chef-Marcus-Samuelsson-Streetbird-Red-Rooster-Harlem-Tips-on-Roasting-Brining-a-Perfect-Chicken-Rotisserie-Recipe1.gif')")
 
