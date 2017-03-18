5c801a131f6d263bdc4b7e6b54fe0aab311e79d4
Adam Domingoes <adamdom@wharton.upenn.edu>
2017-03-18 15:26:22 -0400

    typo fix (sorry, theres a lot of these lol)

--- when2meat.js
+++ when2meat.js
@@ -41,7 +41,7 @@ const when2Meat = function() {
 
     list = $("[id*='GroupTime']").not("[style*='background: #ffffff']").not("[style*='background: rgb(255, 255, 255)']")
     list.each(function(index, elem) {
-      $(elem).css({"background": "rgba(255,255,255,"+ (getIntensityFromStyle(elem.attr("style"))/255).toFixed(2)  +")"})
+      $(elem).css({"background": "rgba(255,255,255,"+ (getIntensityFromStyle($(elem).attr("style"))/255).toFixed(2)  +")"})
     });
   
     $("#YouGrid > div:last-child").css("background", "url('" + baconPattern  + "')")
